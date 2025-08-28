1a315b83106a59e99825ad5e4feee8a1991386c0
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-29 03:20:33 +0530

    Refactor TRPC client and router setup for improved structure and consistency
    - Updated `client.tsx` to standardize string quotes and ensure consistent formatting.
    - Modified `query-client.ts` to enable data deserialization with `superjson`.
    - Simplified `appRouter` in `_app.ts` by removing unused procedures and integrating `categoriesRouter` for better modularity.

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 // ^-- to make sure we can mount the Provider from a server component
-import type { QueryClient } from '@tanstack/react-query';
-import superjson from 'superjson';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
-import { createTRPCReact } from '@trpc/react-query';
-import { useState } from 'react';
-import { makeQueryClient } from './query-client';
-import type { AppRouter } from './routers/_app';
+import type { QueryClient } from "@tanstack/react-query";
+import superjson from "superjson";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import { useState } from "react";
+import { makeQueryClient } from "./query-client";
+import type { AppRouter } from "./routers/_app";
 
 export const trpc = createTRPCReact<AppRouter>();
 
 let clientQueryClientSingleton: QueryClient;
 
 function getQueryClient() {
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // Server: always make a new query client
     return makeQueryClient();
   }
@@ -24,10 +24,10 @@ function getQueryClient() {
 
 function getUrl() {
   const base = (() => {
-    if (typeof window !== 'undefined') return '';
+    if (typeof window !== "undefined") return "";
     // TODO: Modify the URL for outside vercel deployment
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-    return 'http://localhost:3000';
+    return "http://localhost:3000";
   })();
   return `${base}/api/trpc`;
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,26 +1,9 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter } from '../init';
+import { categoriesRouter } from '@/modules/categories/server/procedures';
 
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query(async (opts) => {
-      console.log("using ctx:-", opts.ctx.clerkUserId)
-      return {
-        greeting: ` Hello ${opts.input.text}`,
-      };
-    }),
-  getUser: protectedProcedure.query(async (opts) => {
-    console.log("From ctx:-")
-    console.dir(opts.ctx.dbUser)
-
-    return opts.ctx
-  }),
+  categories: categoriesRouter,
 });
 
 // export type definition of API
