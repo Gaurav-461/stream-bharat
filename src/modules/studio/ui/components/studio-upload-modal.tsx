8b9a40103a281c0431634e2b706dc29c849ad73f
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-09-20 02:40:39 +0530

    Implement video upload modal with TRPC integration
    - Added `StudioUploadModal` component for video uploads, utilizing TRPC for mutation handling.
    - Integrated `ResponsiveDialog` for modal display and `StudioUploader` for video processing.
    - Included loading state and success/error notifications using `toast` for user feedback.

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,13 +1,50 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
+import { ResponsiveDialog } from "@/components/responsive-dailog";
+import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
+import { Loader2Icon, PlusIcon } from "lucide-react";
+import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
-    return (
-        <Button variant="secondary">
-            <PlusIcon />
-            Create
-        </Button>
-    )
-}
+  const utils = trpc.useUtils();
+  const create = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      toast.success("Video created successfully");
+      utils.studio.getMany.invalidate();
+    },
+    onError: (error) => {
+      toast.error("Something went wrong while creating video");
+      console.log("Error creating video:-", error);
+    },
+  });
+  return (
+    <>
+      <ResponsiveDialog
+        title="Upload Video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon className="animate-spin" />
+        )}
+      </ResponsiveDialog>
+
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
+  );
+};
