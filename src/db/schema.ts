b7f50ed5f3f3270ed3eb54875168fd46e9dc174d
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-09-19 14:38:10 +0530

    Enhance database schema with new relations and visibility enum
    - Added `usersRelations` and `categoriesRelations` to establish relationships between users, categories, and videos.
    - Introduced `visibility` enum for video visibility options.
    - Expanded `videos` table schema with additional fields and relationships to users and categories.
    - Updated `categories` table to ensure consistent field definitions.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,19 +1,83 @@
-import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+export const users = pgTable(
+  "users",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull(),
     clerkId: text("clerk_id").unique().notNull(),
-    //TODO: add more fields in future
+    //TODO: add banner fields in future
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
-}, (table) => [uniqueIndex("users_clerk_id_idx").on(table.clerkId)]);
+  },
+  (table) => [uniqueIndex("users_clerk_id_idx").on(table.clerkId)],
+);
 
-export const categories = pgTable("categories", {
+export const usersRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const categories = pgTable(
+  "categories",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull(),
     description: text("description"),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at").notNull().defaultNow(),
-}, (table) => [uniqueIndex("categories_name_idx").on(table.name)]);
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (table) => [uniqueIndex("categories_name_idx").on(table.name)],
+);
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const visibility = pgEnum("visibility", ["Public", "Private"])
+
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  description: text("description"),
+  muxStatus: text("mux_status"),
+  muxAssetId: text("mux_asset_id").unique(),
+  muxUploadId: text("mux_upload_id").unique(),
+  muxPlaybackId: text("mux_playback_id").unique(),
+  muxTrackId: text("mux_track_id").unique(),
+  muxTrackStatus: text("mux_track_status"),
+  thumbnailUrl: text("thumbnail_url"),
+  previewUrl: text("preview_url"),
+  duration: integer("duration").default(0).notNull(),
+  visibility: visibility("visibility").default("Private"),
+  userId: uuid("user_id")
+    .references(() => users.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: uuid("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const videosRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}));
