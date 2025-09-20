7a59b0559317182f70abc3e1b1f80913e1a38348
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-09-21 03:37:40 +0530

    Refactor StudioView component layout by removing unnecessary padding
    - Adjusted the layout of the `StudioView` component by removing extra padding from the main container.
    - Improved overall spacing for a cleaner UI presentation.

--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -2,7 +2,7 @@ import { VideoSection } from "../sections/videos-section";
 
 const StudioView = () => {
     return (
-        <div className="w-full flex flex-col gap-y-6 px-4 pt-2.5 mb-10" >
+        <div className="w-full flex flex-col gap-y-6 pt-2.5 mb-10" >
             <div className="px-4">
                 <h1 className="text-2xl font-bold">Channel content</h1>
                 <p className="text-xs text-muted-foreground">Manage your content and videos</p>
