405b37b78e297c2083af100bacf508af4a642922
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-09-21 03:36:55 +0530

    Add Mux dependencies and configure remote image patterns
    - Added `@mux/mux-node` and `@mux/mux-uploader-react` to dependencies in `package.json` and `bun.lock`.
    - Updated `next.config.ts` to allow remote images from Mux, enhancing image handling capabilities.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {hostname: "image.mux.com"}
+    ]
+  }
 };
 
 export default nextConfig;
