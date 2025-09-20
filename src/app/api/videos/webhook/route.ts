4fc7237471f5cccb2691cb2953002cf793b4bbc6
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-09-21 03:35:51 +0530

    Add webhook route for Mux video events
    - Introduced a new `route.ts` file to handle incoming webhooks from Mux for various video events including creation, readiness, errors, deletion, and track readiness.
    - Implemented signature verification and response handling for each event type.
    - Updated the database with relevant video information based on the webhook payload.

---
+++ src/app/api/videos/webhook/route.ts
@@ -0,0 +1,155 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { mux } from "@/lib/mux";
+import db from "@/db";
+import {
+  VideoAssetCreatedWebhookEvent,
+  VideoAssetErroredWebhookEvent,
+  VideoAssetReadyWebhookEvent,
+  VideoAssetTrackReadyWebhookEvent,
+  VideoAssetDeletedWebhookEvent,
+} from "@mux/mux-node/resources/webhooks";
+import { videos } from "@/db/schema";
+
+const SIGNING_SECRET = process.env.MUX_WEBHOOK_SECRET!;
+
+type WebhookEvent =
+  | VideoAssetCreatedWebhookEvent
+  | VideoAssetErroredWebhookEvent
+  | VideoAssetReadyWebhookEvent
+  | VideoAssetTrackReadyWebhookEvent
+  | VideoAssetDeletedWebhookEvent;
+
+export const POST = async (req: Request) => {
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      "Please add a MUX_WEBHOOK_SECRET from mux dashboard to .env or .env.local:",
+    );
+  }
+
+  const headerPayload = await headers();
+  const muxSignature = headerPayload.get("mux-signature");
+
+  if (!muxSignature) {
+    return new Response("No signature found", { status: 401 });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  mux.webhooks.verifySignature(body, headerPayload, SIGNING_SECRET);
+
+  switch (payload.type as WebhookEvent["type"]) {
+    case "video.asset.created":
+      const data = payload.data as VideoAssetCreatedWebhookEvent["data"];
+
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 });
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxAssetId: data.id,
+          muxStatus: data.status,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+
+      // console.log("✅Video created");
+
+      break;
+
+    case "video.asset.ready": {
+      const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+      const playbackId = data.playback_ids?.[0].id;
+
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 });
+      }
+
+      if (!playbackId) {
+        return new Response("Missing playback ID", { status: 400 });
+      }
+
+      const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.png`;
+      const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+
+      const duration = data.duration ? Math.round(data.duration * 1000) : 0;
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status,
+          muxPlaybackId: playbackId,
+          muxAssetId: data.id,
+          thumbnailUrl,
+          previewUrl,
+          duration,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+
+      // console.log("✅Video ready");
+
+      break;
+    }
+
+    case "video.asset.errored": {
+      const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+      console.log("❌Video errored");
+
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 });
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+
+      break;
+    }
+
+    case "video.asset.deleted": {
+      const data = payload.data as VideoAssetDeletedWebhookEvent["data"];
+      // console.log("✅Video deleted");
+
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 });
+      }
+
+      await db.delete(videos).where(eq(videos.muxUploadId, data.upload_id));
+
+      break;
+    }
+
+    case "video.asset.track.ready": {
+      const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+        asset_id: string;
+      };
+
+      // Typescript incorrectly says that asset_id does not exist
+      const assetId = data.asset_id;
+      const trackId = data.id;
+      const status = data.status;
+
+      if (!assetId) {
+        return new Response("Missing asset ID", { status: 400 });
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxTrackId: trackId,
+          muxTrackStatus: status,
+        })
+        .where(eq(videos.muxAssetId, assetId));
+      // console.log("✅Track Ready");
+
+      break;
+    }
+  }
+
+  return new Response("webhook received", { status: 200 });
+};
