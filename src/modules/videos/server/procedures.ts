afadc55e32cbcfecd72b9a028c17fc099f225582
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-11-01 21:19:06 +0530

    Implement video update and removal procedures in videosRouter

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,43 +1,96 @@
 import db from "@/db";
-import { videos } from "@/db/schema";
+import { videos, videoUpdateSchema } from "@/db/schema";
 import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-
+import { and, eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import z from "zod";
 
 export const videosRouter = createTRPCRouter({
-    create: protectedProcedure.mutation(async ({ ctx }) => {
-        const { id: userId } = ctx.dbUser;
+  create: protectedProcedure.mutation(async ({ ctx }) => {
+    const { id: userId } = ctx.dbUser;
 
-        const upload = await mux.video.uploads.create({
-            new_asset_settings: {
-                passthrough: userId,
-                playback_policy: ["public"],
-                input: [
-                    {
-                        generated_subtitles: [
-                            {
-                                language_code: "en",
-                                name: "English"
-                            }
-                        ]
-                    }
-                ]
-            },
-            cors_origin: "*", // TODO: In production set to web url
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        input: [
+          {
+            generated_subtitles: [
+              {
+                language_code: "en",
+                name: "English",
+              },
+            ],
+          },
+        ],
+      },
+      cors_origin: "*", // TODO: In production set to web url
+    });
+
+    const [video] = await db
+      .insert(videos)
+      .values({
+        userId,
+        title: "New Video",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
+      })
+      .returning();
+
+    console.log("video created:-", video);
+
+    return {
+      video: video,
+      url: upload.url,
+    };
+  }),
+  update: protectedProcedure
+    .input(videoUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.dbUser;
+
+      if (!input.id) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const [updatedVideo] = await db
+        .update(videos)
+        .set({
+          title: input.title,
+          description: input.description,
+          categoryId: input.categoryId,
+          visibility: input.visibility,
+          updatedAt: new Date(),
         })
+        .where(and(eq(videos.id, input.id), eq(videos.userId, userId)))
+        .returning();
 
-        const [video] = await db.insert(videos).values({
-            userId,
-            title: "New Video",
-            muxStatus: "waiting",
-            muxUploadId: upload.id,
-        }).returning();
+      if (!updatedVideo) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
 
-        console.log("video created:-",video)
+      return updatedVideo;
+    }),
+  remove: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.dbUser;
+      const videoId = input.id;
 
-        return {
-            video: video,
-            url: upload.url,
-        }
-    })
-})
+      if (!videoId) {
+        throw new TRPCError({ code: "BAD_REQUEST" });
+      }
+
+      const [removedVideo] = await db
+        .delete(videos)
+        .where(and(eq(videos.id, videoId), eq(videos.userId, userId)))
+        .returning();
+
+      if (!removedVideo) {
+        throw new TRPCError({ code: "NOT_FOUND"})
+      }
+
+      return removedVideo;
+    }),
+});
